855ef2e3d98e7fb308bf0335e6b74d190b893775
robberbadeia <robber.badeia@gmail.com>
2023-10-18 13:56:17 +0300

    up

--- monty.c
+++ monty.c
@@ -15,13 +15,13 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		printf("USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
-		printf("Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	buf = malloc(sizeof(char) * 1000);
