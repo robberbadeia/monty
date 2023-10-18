c8b937cc2d1003585c37bfc5ec46eb7dae00567c
robberbadeia <robber.badeia@gmail.com>
2023-10-18 14:12:16 +0300

    up

--- nop.c
+++ nop.c
@@ -6,12 +6,6 @@
  */
 void nop(stack_t **h, unsigned int line)
 {
-	if (!h || !(*h))
-	{
-		fprintf(stderr, "L%u: can't npo an empty stack\n", line);
-		free_dlist(h);
-		exit(EXIT_FAILURE);
-	}
 	(void) h;
 	(void) line;
 }
